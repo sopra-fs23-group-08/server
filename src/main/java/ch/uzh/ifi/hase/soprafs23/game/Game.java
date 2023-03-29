4a7a12c273a1ccbb605297263b5bb4b21e96d36d
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2023-03-29 16:41:16 +0200

    Created files for game component

---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/Game.java
@@ -0,0 +1,9 @@
+package ch.uzh.ifi.hase.soprafs23.game;
+
+import ch.uzh.ifi.hase.soprafs23.entity.Player;
+import ch.uzh.ifi.hase.soprafs23.entity.Comment;
+
+public class Game {
+
+    
+}
---
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/game/PlayerData.java
@@ -0,0 +1,5 @@
+package ch.uzh.ifi.hase.soprafs23.game;
+
+class PlayerData { //protected (Package Private)
+    
+}
