e9015f3ba00e3b62f5c6caa6efc380464af4de2f
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2023-04-27 13:28:26 +0200

    implementing test

--- src/test/java/ch/uzh/ifi/hase/soprafs23/controller/GameControllerTest.java
+++ src/test/java/ch/uzh/ifi/hase/soprafs23/controller/GameControllerTest.java
@@ -16,17 +16,31 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class GameControllerTest {
     GameService gameService;
+    String gameId;
 
-    @BeforeAll
+    @BeforeEach
     public void setup() {
         gameService = new GameService();
+        gameId = gameService.createGame(new Player("A"));
     }
 
     @Test
     public void createGameTest() {
         var p = new Player("A", "A");
-        var gameId = gameService.createGame(p);
-        assertNotEquals(null, gameId);
-        assertEquals(gameService.getHost(gameId),p);
+        var localGameId = gameService.createGame(p);
+        assertNotEquals(null, localGameId);
+        assertEquals(gameService.getHost(localGameId), p);
+    }
+    
+    @Test
+    public void getHostTest() {
+        assertEquals("A", gameService.getHost(gameId).getName());
+    }
+
+    @Test
+    public void addPlayer() {
+        gameService.addPlayer(gameId, new Player("B"));
+        var lp = gameService.getPlayers(gameId);
+        
     }
 }
