cd75879f9879594676ebb7c4379ca7a3f8420604
Serafin Schoch <30957606+S3r4f1n@users.noreply.github.com>
2023-05-15 17:23:20 +0200

    multithreading nono

--- src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameController.java
+++ src/main/java/ch/uzh/ifi/hase/soprafs23/controller/GameController.java
@@ -42,7 +42,7 @@ public class GameController {
     @PostMapping("/games")
     @ResponseStatus(HttpStatus.CREATED)
     @ResponseBody
-    public String createGame(@RequestBody PlayerDTO playerDTO) {
+    public synchronized String createGame(@RequestBody PlayerDTO playerDTO) {
         MutablePlayer player = DTOMapper.INSTANCE.convertPlayerDTOtoEntity(playerDTO);
         String gameId = gameService.createGame(new Player(player));
         return String.format("{\"id\":\"%s\"}", gameId);
@@ -50,32 +50,34 @@ public class GameController {
 
     @GetMapping("/games/{gameId}/lobby")
     @ResponseStatus(HttpStatus.OK)
-    public void isLobbyJoinable(@PathVariable String gameId) {
+    public synchronized void isLobbyJoinable(@PathVariable String gameId) {
         gameService.isLobbyJoinable(gameId);
     }
     @PostMapping("/games/helpers/playlist")
     @ResponseStatus(HttpStatus.OK)
-    public void checkPlaylistUrl(@RequestBody PlaylistDTO playlistDTO) {
+    public synchronized void checkPlaylistUrl(@RequestBody PlaylistDTO playlistDTO) {
         gameService.checkPlaylist(playlistDTO.getPlaylistUrl());
     }
 
     @GetMapping("/games/{gameId}/host")
     @ResponseStatus(HttpStatus.OK)
     @ResponseBody
-    public PlayerDTO getHost(@PathVariable String gameId) {
+    public synchronized PlayerDTO getHost(@PathVariable String gameId) {
         MutablePlayer host = gameService.getHost(gameId);
         return DTOMapper.INSTANCE.convertEntityToPlayerDTO(host);
     }
 
     @MessageMapping("/games/{gameId}/players/add")
     @SendTo("/topic/games/{gameId}/players")
-    public Collection<PlayerWsDTO> addPlayer(@DestinationVariable String gameId, PlayerDTO playerDTO) {
+    public synchronized Collection<PlayerWsDTO> addPlayer(@DestinationVariable String gameId, PlayerDTO playerDTO) {
         try {
             MutablePlayer player = DTOMapper.INSTANCE.convertPlayerDTOtoEntity(playerDTO);
             // add player to game
             gameService.isLobbyJoinable(gameId);
             gameService.addPlayer(gameId, new Player(player));
             // convert player-list to DTOs
+        } catch (ResponseStatusException e) {
+            messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
@@ -85,14 +87,15 @@ public class GameController {
 
     @MessageMapping("/games/{gameId}/players/remove")
     @SendTo("/topic/games/{gameId}/players")
-    public Collection<PlayerWsDTO> removePlayer(@DestinationVariable String gameId, PlayerDTO playerDTO) {
+    public synchronized Collection<PlayerWsDTO> removePlayer(@DestinationVariable String gameId, PlayerDTO playerDTO) {
         try {
             // convert DTO to entity
             MutablePlayer player = DTOMapper.INSTANCE.convertPlayerDTOtoEntity(playerDTO);
             // remove player from game
             gameService.removePlayer(gameId, new Player(player));
             // convert player-list to DTOs
-            
+        } catch (ResponseStatusException e) {
+                    messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
@@ -101,11 +104,13 @@ public class GameController {
 
     @MessageMapping("/games/{gameId}/settings")
     @SendTo("/topic/games/{gameId}/settings")
-    public SettingsWsDTO updateSettings(@DestinationVariable String gameId, SettingsWsDTO settingsWsDTO) {
+    public synchronized SettingsWsDTO updateSettings(@DestinationVariable String gameId, SettingsWsDTO settingsWsDTO) {
         try {
             // update settings
             gameService.setGameSettings(gameId, settingsWsDTO);
             // send new settings to all players
+        } catch (ResponseStatusException e) {
+            messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
@@ -114,10 +119,12 @@ public class GameController {
 
     @MessageMapping("/games/{gameId}/start")
     @SendTo("/topic/games/{gameId}/start")
-    public String startGame(@DestinationVariable String gameId) {
+    public synchronized String startGame(@DestinationVariable String gameId) {
         // start game
         try {
             gameService.startGame(gameId);
+        } catch (ResponseStatusException e) {
+            messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
@@ -125,28 +132,32 @@ public class GameController {
     }
 
     @MessageMapping("/games/{gameId}/end")
-    public void endGame(@DestinationVariable String gameId) {
+    public synchronized void endGame(@DestinationVariable String gameId) {
         // end game
         // TODO create gameService method & notify all players
         // gameService.endGame(gameId);
     }
 
     @MessageMapping("/games/{gameId}/players/{playerToken}/decision")
-    public void handlePlayerDecision(@DestinationVariable String gameId,
+    public synchronized void handlePlayerDecision(@DestinationVariable String gameId,
                                      @DestinationVariable String playerToken,
                                      DecisionWsDTO decisionWsDTO)
     {
         try {
             gameService.playerDecision(gameId, playerToken, decisionWsDTO);
+        } catch (ResponseStatusException e) {
+            messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
     }
 
     @MessageMapping("/games/{gameId}/rounds/next")
-    public void nextRound(@DestinationVariable String gameId) {
+    public synchronized void nextRound(@DestinationVariable String gameId) {
         try {
             gameService.nextRound(gameId);
+        } catch (ResponseStatusException e) {
+            messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", new Exception(e.getMessage(), e.getCause()));
         } catch (Exception e) {
             messagingTemplate.convertAndSend("/topic/games/" + gameId + "/error", e);
         }
@@ -154,17 +165,17 @@ public class GameController {
 
     /** OBSERVER ENDPOINT METHODS
      * these methods are invoked by gameService */
-    public void gameStateChanged(String gameId, GameStateWsDTO gameStateWsDTO) {
+    public synchronized void gameStateChanged(String gameId, GameStateWsDTO gameStateWsDTO) {
         String destination = String.format("/topic/games/%s/state", gameId);
         messagingTemplate.convertAndSend(destination, gameStateWsDTO);
     }
 
-    public void playerStateChanged(String gameId, Collection<PlayerWsDTO> playersDTOList) {
+    public synchronized void playerStateChanged(String gameId, Collection<PlayerWsDTO> playersDTOList) {
         String destination = String.format("/topic/games/%s/players", gameId);
         messagingTemplate.convertAndSend(destination, playersDTOList);
     }
 
-    public void newHand(String gameId, Player player, Hand hand) {
+    public synchronized void newHand(String gameId, Player player, Hand hand) {
         String destination = String.format("/topic/games/%s/players/%s/hand", gameId, player.getToken());
         ObjectMapper objectMapper = new ObjectMapper();
         String responseBody;
@@ -178,17 +189,17 @@ public class GameController {
         messagingTemplate.convertAndSend(destination, responseBody);
     }
 
-    public void newVideoData(String gameId, VideoDataWsDTO videoData) {
+    public synchronized void newVideoData(String gameId, VideoDataWsDTO videoData) {
         String destination = String.format("/topic/games/%s/video", gameId);
         messagingTemplate.convertAndSend(destination, videoData);
     }
 
     @MessageMapping("/echo")
-    public void echo(String msg) {
+    public synchronized void echo(String msg) {
         echoResponse(msg);
     }
 
-    public void echoResponse(String msg) {
+    public synchronized void echoResponse(String msg) {
         messagingTemplate.convertAndSend("/topic/echo", "pong" + msg);
     }
 
@@ -198,7 +209,7 @@ public class GameController {
      * 
      */
     @MessageMapping("/echoDTO")
-    public void settingsEcho() throws MessagingException, JsonProcessingException {
+    public synchronized void settingsEcho() throws MessagingException, JsonProcessingException {
         
         var player = new PlayerWsDTO("playerToken", "Peter Toggenburger", 0, Decision.NOT_DECIDED, false, false, false);
         Collection<PlayerWsDTO> playerCollection = new ArrayList<>();
@@ -213,6 +224,7 @@ public class GameController {
         messagingTemplate.convertAndSend("/topic/echoDecision", new DecisionWsDTO());
         messagingTemplate.convertAndSend("/topic/echoSettings", new SettingsWsDTO());
         messagingTemplate.convertAndSend("/topic/echoHand", objectMapper.writeValueAsString(hand.getComments()));
+        messagingTemplate.convertAndSend("/topic/echoErrorA", new IllegalStateException("Test error"));
     }
 }
 
